cc635f3360b1efbd9923b435c39e034c9194bc4f
Chenry <henrychen0214@gmail.com>
2023-11-01 19:00:13 +0800

    asdf

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,7 +8,9 @@ void autonomous() {
     using drive::linearScurveProfile;
     using namespace drive;
     
-    RRLib::CubicBezier testPath({0, 0, 0}, {1, 0, 0});
-    RRLib::DiscretePath testPathDiscrete(testPath.generatePathByLengthWithCurvature(0.05));
-    ppController.followPath(testPathDiscrete);
+    // RRLib::CubicBezier testPath({0, 0, 0}, {1, 0, 0});
+    // RRLib::DiscretePath testPathDiscrete(testPath.generatePathByLengthWithCurvature(0.05));
+    // ppController.followPath(testPathDiscrete);
+    linearScurveProfile.moveForwards(2_m);
+    linearScurveProfile.printVectorPosition(linearScurveProfile.pathTrajectory);
 }
